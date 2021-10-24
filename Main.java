75e65aa001d26e9fbbb942ff0cf6a756a399e36f
shyianenko <93085101+shyianenko@users.noreply.github.com>
2021-10-24 20:50:43 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -14,19 +14,19 @@ public class Main {
 
 		while (sc.next().equals("Yes")) {
 			student = new Student();
-//			System.out.println("Input name student");
-//			student.setName(sc.next());
+			System.out.println("Input name student");
+			student.setName(sc.next());
 			System.out.println("Input second name student");
 			student.setSecondName(sc.next());
-//			System.out.println("Input age student");
-//			student.setAge(sc.nextInt());
-//			System.out.println("Input sex student");
-//			Sex sex = sc.next().equals("Male") ? Sex.MALE : Sex.FEMALE;
-//			student.setSex(sex);
-//			System.out.println("Input id student");
-//			student.setId(sc.nextLong());
-//			System.out.println("Input students Group name");
-//			student.setGroupName(sc.next());
+			System.out.println("Input age student");
+			student.setAge(sc.nextInt());
+			System.out.println("Input sex student");
+			Sex sex = sc.next().equals("Male") ? Sex.MALE : Sex.FEMALE;
+			student.setSex(sex);
+			System.out.println("Input id student");
+			student.setId(sc.nextLong());
+			System.out.println("Input students Group name");
+			student.setGroupName(sc.next());
 			//Yesgroup.addStudent(student);
 			
 			group.addStudent(student);
